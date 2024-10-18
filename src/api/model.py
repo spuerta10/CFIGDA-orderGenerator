69ab06f3e922365bbb97815ee4a80a2978eb9d38
Santiago Puerta <puertasantiago2002@gmail.com>
2024-10-18 11:19:03 -0500

    docs: Added pydoc to module's functions

--- src/api/model.py
+++ src/api/model.py
@@ -9,12 +9,24 @@ class Model:
         orders_to_generate: int,
         order_generator: OrderGenerator,
         publisher: Publisher
-    ) -> 'Model':
-        orders = []  # TODO: delete this 
+    ) -> list[str]:
+        """
+        Generates a specified number of orders using the provided order generator,
+        publishes each order using the provided publisher, and returns a list of the generated order IDs.
+
+        Args:
+            orders_to_generate (int): The number of orders to generate and publish.
+            order_generator (OrderGenerator): An instance responsible for generating order dictionaries.
+            publisher (Publisher): An instance responsible for publishing the generated orders.
+
+        Returns:
+            list[str]: A list of order IDs from the generated and published orders.
+        """
+        orders: list[str] = []
         for _ in range(orders_to_generate):
            order: dict= order_generator.generate()
-           #publisher.publish(order)
-           orders.append(order)  # TODO: delete this
+           publisher.publish(order)
+           orders.append(order["order_id"])
            
         return orders
     
