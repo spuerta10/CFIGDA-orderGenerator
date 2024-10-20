1f41e7efc1e917cfdc3a3f391a164610d4db6843
spuerta10 <puertasantiago2002@gmail.com>
2024-10-20 00:40:11 -0500

    docs: Added docs to OrderGenerator methods

--- src/order_publisher/generator.py
+++ src/order_publisher/generator.py
@@ -10,10 +10,26 @@ from pydantic import BaseModel
 
 class OrderGenerator:
     def __init__(self, generator_conf_path: str, generator_conf_schema: BaseModel = None): 
+        """Initialize the OrderGenerator with configuration.
+
+        Args:
+            generator_conf_path (str): The path to the configuration file for the generator.
+            generator_conf_schema (BaseModel, optional): An optional schema for validating the configuration.
+                Defaults to None.
+        """
         self._conf: Config = ConfigFactory.get_conf(generator_conf_path, generator_conf_schema)
     
     
     def generate(self) -> dict:
+        """Generate a random order.
+
+        This method creates an order dictionary with a unique order ID, a random customer ID,
+        a list of order items, and a random city ID based on the configuration settings.
+
+        Returns:
+            dict: A dictionary representing the generated order, containing keys such as 
+            'customer_id', 'order_id', 'order_items', and 'city_id'.
+        """
         order = {
             "customer_id": randint(self._conf.customer["customer_id_min"],self._conf.customer["customer_id_max"]),
             "order_id": str(uuid4()),  # generate an UUID 4 unique order identifier
@@ -24,6 +40,14 @@ class OrderGenerator:
     
     
     def _generate_order_items(self) -> list[dict]:
+        """Generate a list of order items.
+
+        This method creates a list of dictionaries, where each dictionary represents an order item
+        with a randomly assigned item ID and a list of associated tags.
+
+        Returns:
+            list[dict]: A list of dictionaries, each containing 'item_id' and 'item_tags'.
+        """
         items_number =  randint(
             self._conf.order_items["min_order_items"],
             self._conf.order_items["max_order_items"]
@@ -39,6 +63,14 @@ class OrderGenerator:
 
 
     def _generate_item_tags(self) -> list[int]:
+        """Generate a list of item tags.
+
+        This method creates a list of integers, where each integer represents a tag ID.
+        The number of tags is randomly determined based on configuration settings.
+
+        Returns:
+            list[int]: A list of integers representing tag IDs associated with an item.
+        """
         tags_number = randint(
            self._conf.tags["min_tags"],
            self._conf.tags["max_tags"] 
